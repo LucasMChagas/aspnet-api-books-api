17bb5d68abf80f7b3605bd1affc1f3b922c7c376
Lucas Miranda <lucas_miranda_chagas@hotmail.com>
2024-04-14 11:47:35 -0300

    Criação da estrutura do projeto e  do banco de dados

---
+++ BooksApi/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using BooksApi.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BooksApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {            
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
